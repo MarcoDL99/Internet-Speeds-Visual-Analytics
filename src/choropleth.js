ed98b7ac34df83f715d40af5555cb931c42b84a9
sda <marcodl722@gmail.com>
2023-03-17 16:35:05 +0100

    edits

---
+++ src/choropleth.js
@@ -0,0 +1,6 @@
+var citiesMapPath="./maps_geojson/limits_IT_municipalities.geojson"
+var provincesMapPath="./maps_geojson/limits_IT_provinces.geojson"
+var regionsMapPath="./maps_geojson/limits_IT_regions.geojson"
+
+var datasetPath="./dataset/speedtest_bul_tiles.csv"
+
